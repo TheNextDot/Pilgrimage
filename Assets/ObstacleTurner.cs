316e6d2a4b3423793df430c53fea6a5c03e796b4
MarcD12345 <marc@derksen.me>
2021-03-21 20:16:40 +0100

    Background, emissive eyes, better obstacles

---
+++ Assets/ObstacleTurner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleTurner : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 20, 0);
+    }
+}
